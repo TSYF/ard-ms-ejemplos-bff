e18a3b0eb7fe3b98274f8964b43010a3cd623317
Tomás <tsyf1999@gmail.com>
2024-06-01 03:52:10 -0400

    env HOSTNAME used for listening
    package name also corrected

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,6 +3,7 @@ import { get } from 'env-var';
 
 export const envs = {
     PORT: get("PORT").required().asPortNumber(),
+    HOSTNAME: get("HOSTNAME").asString(),
     EXAMPLE_ENDPOINT: get("EXAMPLE_ENDPOINT").required().asUrlString(),
     DEFAULT_API_PREFIX: get("DEFAULT_API_PREFIX").required().asString(),
 }
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ const app = express();
 
 app.use(morgan("combined"))
 app.use(express.json());
-const { PORT, DEFAULT_API_PREFIX } = envs;
+const { PORT, HOSTNAME, DEFAULT_API_PREFIX } = envs;
 
 app.use(DEFAULT_API_PREFIX, exampleRouter);
-app.listen(PORT || 8000, () => console.log("MS-EJEMPLOS-BFF STARTED"));
+
+app.listen(PORT || 8000, HOSTNAME || "127.0.0.1", () => console.log("MS-EJEMPLOS-BFF STARTED"));
